528921e29705e395dde7898a04eca57b32d4f5aa
Martin Büttner <m.buettner.d@gmail.com>
2013-09-05 19:03:16 +0200

    Skip affected tests if encoding conversion is not possible (instead of throwing an Exception).

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -31,20 +31,24 @@ class BaseTestCase extends \PHPUnit_Framework_TestCase
             );
         });
 
-        $this->dic['encodingConverter'] = $this->dic->share(function($c) {
-            if (extension_loaded('iconv'))
-                $type = 'Core\IConvEncodingConverter';
-            else if (function_exists('utf8_encode')
-                     && function_exists('utf8_decode'))
-                $type = 'Core\Utf8EncodingConverter';
-            else
-                throw new Exception('No encoding conversion available.');
+        // $this cannot be passed into closures before PHP 5.4
+        $that = $this;
+        $this->dic['encodingConverter'] = $this->dic->share(
+            function($c) use ($that) {
+                if (extension_loaded('iconv'))
+                    $type = 'Core\IConvEncodingConverter';
+                else if (function_exists('utf8_encode')
+                         && function_exists('utf8_decode'))
+                    $type = 'Core\Utf8EncodingConverter';
+                else
+                    $that->markTestSkipped('No encoding conversion available.');
 
-            return FF::getInstance(
-                $type,
-                $c['loggerClass'],
-                $c['configuration']
-            );
-        });
+                return FF::getInstance(
+                    $type,
+                    $c['loggerClass'],
+                    $c['configuration']
+                );
+            }
+        );
     }
 }
