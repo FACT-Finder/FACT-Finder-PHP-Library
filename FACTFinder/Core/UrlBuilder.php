68d9b4ebdf222ce99cc4e1714eb35a3bfe119078
Martin Büttner <m.buettner.d@gmail.com>
2013-09-06 13:59:26 +0200

    Refactored UrlBuilder.

--- FACTFinder/Core/UrlBuilder.php
+++ FACTFinder/Core/UrlBuilder.php
@@ -1,6 +1,8 @@
 <?php
 namespace FACTFinder\Core;
 
+use FACTFinder\Loader as FF;
+
 /**
  * Assembles URLs for different kinds of authentication based on the given
  * parameters and the configuration.
@@ -18,9 +20,9 @@ class UrlBuilder
     protected $configuration;
 
     /**
-     * @var mixed[]
+     * @var FACTFinder\Util\Parameters
      */
-    protected $parameters = array();
+    protected $parameters;
 
     /**
      * @var string
@@ -32,118 +34,47 @@ class UrlBuilder
      * @param string $loggerClass Class name of logger to use. The class should
      *        implement FACTFinder\Util\LoggerInterface.
      * @param ConfigurationInterface $configuration
-     * @param mixed[] $parameters Optional parameters array to initialize the
-     *        UrlBuild with.
+     * @param FACTFinder\Util\Parameters $parameters Optional parameters object
+     *        to initialize the UrlBuilder with.
      */
     public function __construct(
         $loggerClass,
         ConfigurationInterface $configuration,
-        array $parameters = null
+        FACTFinder\Util\Parameters $parameters = null
     ) {
         $this->log = $loggerClass::getLogger(__CLASS__);
         $this->log->info("Initializing URL Builder.");
 
         $this->configuration = $configuration;
-        if ($parameters != null) $this->parameters = $parameters;
+
+        $this->parameters = $parameters ?: FF::getInstance('Util\Parameters');
     }
 
     /**
-     * Reset all parameters
+     * Set the action to be queried on the FACT-Finder server. e.g. "Search.ff"
      *
-     * @param mixed[] $parameters
+     * @param string $action
      */
-    public function resetParameters(array $parameters)
-    {
-        $this->parameters = $parameters;
-    }
-
-    /**
-     * Reset a single parameter
-     *
-     * @param string $name
-     * @param mixed $value
-     */
-    public function setParameter($name, $value)
-    {
-        $this->parameters[$name] = $value;
-    }
-
-    /**
-     * Set all given parameters. Previous values of the given parameters will be
-     * replaced. Unmentioned parameters will retain their values.
-     *
-     * @param mixed[] $parameters
-     */
-    public function setParameters(array $parameters)
-    {
-        $this->parameters = array_merge($this->parameters, $parameters);
-    }
-
-    /**
-     * Adds a single parameter. If there it already has one or more values, the
-     * new one will be added in addition to the old one(s).
-     *
-     * @param string $name
-     * @param mixed $value
-     */
-    public function addParameter($name, $value)
-    {
-        if(!isset($this->parameters[$name]))
-            $this->parameters[$name] = $value;
-        else
-        {
-            if (is_array($this->parameters[$name]))
-                array_push($this->parameters[$name], $value);
-            else
-                $this->parameters[$name] = array(
-                    $this->parameters[$name],
-                    $value
-                );
-        }
-    }
-
-    /**
-     * Adds all given parameters in addition to all existing values.
-     *
-     * @param mixed[] $parameters
-     */
-    public function addParameters(array $parameters)
-    {
-        foreach ($parameters as $k => $v)
-            $this->addParameter($k, $v);
-    }
-
-    /**
-     * Unset a single parameter
-     *
-     * @param string $name
-     */
-    public function unsetParameter($name)
-    {
-        unset($this->parameters[$name]);
-    }
-
-    /**
-     * Unset all parameters. This is just a convenience method and could also be
-     * done with resetParameters().
-     */
-    public function unsetAllParameters($name)
-    {
-        $this->resetParameters(array());
-    }
-
     public function setAction($action)
     {
         $this->action = $action;
     }
 
+    /**
+     * Get the action to be queried on the FACT-Finder server. e.g. "Search.ff"
+     *
+     * @return string
+     */
     public function getAction()
     {
         return $this->action;
     }
 
     /**
-     * @return array
+     * Returns the parameters object used by the UrlBuilder, on which parameters
+     * can be changed.
+     *
+     * @return FACTFinder\Util\Parameters
      */
     public function getParameters()
     {
@@ -151,134 +82,153 @@ class UrlBuilder
     }
 
     /**
-     * get url with advanced authentication encryption
+     * Returns a full URL with authentication data. The type of authentication
+     * is determined from the configuration.
+     * Note that this method may set a channel parameter if there is none
+     * already.
      *
-     * @return string url
+     * @return string The full URL.
+     *
+     * @throws Exception if no valid authentication type was configured.
      */
-    public function getAdvancedAuthenticationUrl() {
+    public function getAuthenticationUrl()
+    {
+        $this->ensureChannelParameter();
+
+        $c = $this->configuration;
+        if ($c->isAdvancedAuthenticationType())
+            return $this->getAdvancedAuthenticationUrl();
+        else if ($c->isSimpleAuthenticationType())
+            return $this->getSimpleAuthenticationUrl();
+        else if ($c->isHttpAuthenticationType())
+            return $this->getHttpAuthenticationUrl();
+        else
+            throw new \Exception('Invalid authentication type configured.');
+    }
+
+    /**
+     * Get URL with advanced authentication encryption.
+     *
+     * @return string The full URL.
+     */
+    protected function getAdvancedAuthenticationUrl()
+    {
         $configuration = $this->configuration;
         $parameters = $this->parameters;
-
-        $channel = $this->getChannel($parameters, $configuration);
-        if ($channel != '') {
-            $parameters['channel'] = $channel;
-        }
 
         $ts         = time() . '000'; //milliseconds needed
         $prefix     = $configuration->getAuthenticationPrefix();
         $postfix    = $configuration->getAuthenticationPostfix();
-        $authParameters = "timestamp=$ts&username=".$configuration->getUserName()
-            . '&password=' . md5($prefix . $ts . md5($configuration->getPassword()) . $postfix);
+        $hashedPW   = md5($prefix
+                    . $ts
+                    . md5($configuration->getPassword())
+                    . $postfix);
+        $authenticationParameters = 'timestamp=' . $ts
+                                  . '&username=' . $configuration->getUserName()
+                                  . '&password=' . $hashedPW;
 
-        $url = $configuration->getRequestProtocol() . '://'
-            . $configuration->getServerAddress() . ':' . $configuration->getServerPort() . '/'
-            . $configuration->getContext() . '/'.$this->action.'?' . http_build_query($parameters, '', '&')
-            . (count($parameters)?'&':'') . $authParameters;
+        $url = $this->buildAddress()
+             . '?' . $parameters->toJavaQueryString()
+             . (count($parameters) ? '&' : '') . $authenticationParameters;
 
-        // The following line removes all []-indices from array parameters, because tomcat doesn't need them
-        $url = preg_replace("/%5B[A-Za-z0-9]*%5D/", "", $url);
-        $this->log->info("Request Url: ".$url);
+        $this->log->info("Request Url: " . $url);
         return $url;
     }
 
     /**
-     * get url with simple authentication encryption
+     * Get URL with simple authentication encryption.
      *
-     * @return string url
+     * @return string The full URL.
      */
-    public function getSimpleAuthenticationUrl() {
+    protected function getSimpleAuthenticationUrl() {
         $configuration = $this->configuration;
         $parameters = $this->parameters;
-
-        $channel = $this->getChannel($parameters, $configuration);
-        if ($channel != '') {
-            $parameters['channel'] = $channel;
-        }
 
         $ts = time() . '000'; //milliseconds needed but won't be considered
-        $authParameters = "timestamp=$ts&username=".$configuration->getUserName()
-            . '&password=' . md5($configuration->getPassword());
+        $authenticationParameters = "timestamp=" . $ts
+                        . '&username=' . $configuration->getUserName()
+                        . '&password=' . md5($configuration->getPassword());
 
-        $url = $configuration->getRequestProtocol() . '://'
-            . $configuration->getServerAddress() . ':' . $configuration->getServerPort() . '/'
-            . $configuration->getContext() . '/'.$this->action.'?' . http_build_query($parameters, '', '&')
-            . (count($parameters)?'&':'') . $authParameters;
+        $url = $this->buildAddress()
+             . '?' . $parameters->toJavaQueryString()
+             . (count($parameters) ? '&' : '') . $authenticationParameters;
 
-        // The following line removes all []-indices from array parameters, because tomcat doesn't need them
-        $url = preg_replace("/%5B[A-Za-z0-9]*%5D/", "", $url);
-        $this->log->info("Request Url: ".$url);
+        $this->log->info("Request Url: " . $url);
         return $url;
     }
 
     /**
-     * get url with http authentication
+     * Get URL with HTTP authentication.
      *
-     * @return string url
+     * @return string The full URL.
      */
-    public function getHttpAuthenticationUrl() {
+    protected function getHttpAuthenticationUrl() {
         $configuration = $this->configuration;
         $parameters = $this->parameters;
 
-        $channel = $this->getChannel($parameters, $configuration);
-        if ($channel != '') {
-            $parameters['channel'] = $channel;
-        }
+        $authentication = sprintf(
+            '%s:%s@',
+            $configuration->getUserName(),
+            $configuration->getPassword()
+        );
+        if ($authentication == ':@') $authentication = '';
 
-        $auth = $configuration->getUserName() . ':' . $configuration->getPassword() . '@';
-        if ($auth == ':@') $auth = '';
+        $url = $this->buildAddress(true)
+             . (count($parameters) ? '?' : '') . $parameters->toJavaQueryString();
 
-        $url = $configuration->getRequestProtocol() . '://' . $auth
-            . $configuration->getServerAddress() . ':' . $configuration->getServerPort() . '/'
-            . $configuration->getContext() . '/' . $this->action . (count($parameters)?'?':'')
-            . http_build_query($parameters, '', '&');
-
-        // The following line removes all []-indices from array parameters, because tomcat doesn't need them
-        $url = preg_replace("/%5B[A-Za-z0-9]*%5D/", "", $url);
-        $this->log->info("Request Url: ".$url);
+        $this->log->info("Request Url: " . $url);
         return $url;
     }
 
     /**
-     * get url with no authentication.
+     * Get URL without authentication data.
      *
-     * @return string url
+     * @return string The full URL.
      */
     public function getNonAuthenticationUrl() {
         $configuration = $this->configuration;
         $parameters = $this->parameters;
 
-        $channel = $this->getChannel($parameters, $configuration);
-        if ($channel != '') {
-            $parameters['channel'] = $channel;
-        }
+        $this->ensureChannelParameter();
 
-        $url = $configuration->getRequestProtocol() . '://'
-            . $configuration->getServerAddress() . ':' . $configuration->getServerPort() . '/'
-            . $configuration->getContext() . '/' . $this->action . (count($parameters)?'?':'')
-            . http_build_query($parameters, '', '&');
+        $url = $this->buildAddress()
+             . (count($parameters) ? '?' : '') . $parameters->toJavaQueryString();
 
-        // The following line removes all []-indices from array parameters, because tomcat doesn't need them
-        $url = preg_replace("/%5B[A-Za-z0-9]*%5D/", "", $url);
-        // Include the following line only for debugging purposes
-        // This method is called quite often for several checking tasks
-        //$this->log->info("Request Url: ".$url);
         return $url;
     }
 
     /**
-     * get channel from parameters or configuration (parameters override configuration)
-     *
-     * @param array $parameters
-     * @param FACTFinder_Abstract_Configuration $configuration
-     * @return string channel
+     * If no channel is set, try to fill it from configuration data.
      */
-    protected function getChannel($parameters, $configuration) {
-        $channel = '';
-        if (isset($parameters['channel']) && strlen($parameters['channel']) > 0) {
-            $channel = $parameters['channel'];
-        } else if($configuration->getChannel() != '') {
-            $channel = $configuration->getChannel();
+    protected function ensureChannelParameter() {
+        if ((!isset($this->parameters['channel'])
+            || $this->parameters['channel'] == '')
+            && $this->configuration->getChannel() != ''
+        ) {
+            $this->parameters['channel'] = $this->configuration->getChannel();
         }
-        return $channel;
+    }
+
+    protected function buildAddress($includeHttpAuthentication = false)
+    {
+        $configuration = $this->configuration;
+
+        $authentication = '';
+        if ($includeHttpAuthentication
+            && $configuration->getUserName() != ''
+            && $configuration->getPassword() != ''
+        ) {
+            $authentication = sprintf(
+                '%s:%s@',
+                $configuration->getUserName(),
+                $configuration->getPassword()
+            );
+        }
+
+        return $configuration->getRequestProtocol() . '://'
+             . $authentication . $configuration->getServerAddress()
+             . ':' . $configuration->getServerPort()
+             . '/' . $configuration->getContext()
+             . '/' . $this->action;
     }
 }
