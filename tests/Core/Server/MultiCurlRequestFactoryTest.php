6e416e0e046b86f2cc0eae8913842f66795633e7
Martin Büttner <m.buettner.d@gmail.com>
2013-12-01 20:23:56 +0000

    Add MultiCurlRequestFactory

--- tests/Core/Server/MultiCurlRequestFactoryTest.php
+++ tests/Core/Server/MultiCurlRequestFactoryTest.php
@@ -3,7 +3,7 @@ namespace FACTFinder\Test\Core\Server;
 
 use FACTFinder\Loader as FF;
 
-class MultiCurlDataProviderTest extends \FACTFinder\Test\BaseTestCase
+class MultiCurlRequestFactoryTest extends \FACTFinder\Test\BaseTestCase
 {
     /**
      * @var FACTFinder\Util\LoggerInterface
@@ -21,21 +21,21 @@ class MultiCurlDataProviderTest extends \FACTFinder\Test\BaseTestCase
     protected $curlStub;
 
     /**
-     * @var FACTFinder\Core\Server\MultiCurlDataProvider
+     * @var FACTFinder\Core\Server\MultiCurlRequestFactory
      */
-    protected $dataProvider;
+    protected $factory;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->curlStub = FF::getInstance('Util\CurlStub');
-        $this->dataProvider = FF::getInstance(
-            'Core\Server\MultiCurlDataProvider',
+        $this->curlStub = self::$dic['curlStub'];
+        $this->factory = FF::getInstance(
+            'Core\Server\MultiCurlRequestFactory',
             self::$dic['loggerClass'],
             self::$dic['configuration'],
-            $this->curlStub,
-            self::$dic['serverUrlBuilder']
+            self::$dic['curlStub'],
+            FF::getInstance('Util\Parameters', array('query' => 'bmx'))
         );
 
         $loggerClass = self::$dic['loggerClass'];
@@ -44,12 +44,12 @@ class MultiCurlDataProviderTest extends \FACTFinder\Test\BaseTestCase
         $this->configuration = self::$dic['configuration'];
     }
 
-    public function testLoadResponse()
+    public function testGetWorkingRequest()
     {
         $this->configuration->makeHttpAuthenticationType();
 
         $requiredOptions = array(
-            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?format=json&do=getTagCloud&verbose=true&channel=de'
+            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?query=bmx&format=json&do=getTagCloud&verbose=true&channel=de'
         );
         $responseContent = 'test response';
         $info = array(
@@ -59,31 +59,30 @@ class MultiCurlDataProviderTest extends \FACTFinder\Test\BaseTestCase
         $this->curlStub->setResponse($responseContent, $requiredOptions);
         $this->curlStub->setInformation($info, $requiredOptions);
 
-        $connectionData = FF::getInstance('Core\Server\ConnectionData');
-        $id = $this->dataProvider->register($connectionData);
+        $request = $this->factory->getRequest();
 
-        $parameters = $connectionData->getParameters();
+        $parameters = $request->getParameters();
 
         $parameters['format'] = 'json';
         $parameters['do'] = 'getTagCloud';
 
-        $connectionData->setAction('TagCloud.ff');
+        $request->setAction('TagCloud.ff');
 
-        $this->dataProvider->loadResponse($id);
-
-        $response = $connectionData->getResponse();
+        $response = $request->getResponse();
         $this->assertEquals(0, $response->getConnectionErrorCode());
         $this->assertEquals('CURLE_OK', $response->getConnectionError());
         $this->assertEquals(200, $response->getHttpCode());
         $this->assertEquals($responseContent, $response->getContent());
     }
 
-    public function testLoadInParallel()
+    // This is how multiple requests should be used: configure all of them
+    // before fetching the first response from one of them.
+    public function testUseRequestsInParallel()
     {
         $this->configuration->makeHttpAuthenticationType();
 
         $requiredOptions1 = array(
-            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?format=json&do=getTagCloud&verbose=true&channel=de'
+            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?query=bmx&format=json&do=getTagCloud&verbose=true&channel=de'
         );
         $responseContent1 = 'test response 1';
         $info = array(
@@ -94,7 +93,7 @@ class MultiCurlDataProviderTest extends \FACTFinder\Test\BaseTestCase
         $this->curlStub->setInformation($info, $requiredOptions1);
 
         $requiredOptions2 = array(
-            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?format=xml&do=getTagCloud&verbose=true&channel=de'
+            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?query=bmx&format=xml&do=getTagCloud&verbose=true&channel=de'
         );
         $responseContent2 = 'test response 2';
         $info = array(
@@ -104,52 +103,47 @@ class MultiCurlDataProviderTest extends \FACTFinder\Test\BaseTestCase
         $this->curlStub->setResponse($responseContent2, $requiredOptions2);
         $this->curlStub->setInformation($info, $requiredOptions2);
 
-        $connectionData1 = FF::getInstance('Core\Server\ConnectionData');
-        $id1 = $this->dataProvider->register($connectionData1);
+        $request1 = $this->factory->getRequest();
 
-        $parameters = $connectionData1->getParameters();
+        $parameters = $request1->getParameters();
 
         $parameters['format'] = 'json';
         $parameters['do'] = 'getTagCloud';
 
-        $connectionData1->setAction('TagCloud.ff');
+        $request1->setAction('TagCloud.ff');
 
-        $connectionData2 = FF::getInstance('Core\Server\ConnectionData');
-        $id2 = $this->dataProvider->register($connectionData2);
+        $request2 = $this->factory->getRequest();
 
-        $parameters = $connectionData2->getParameters();
+        $parameters = $request2->getParameters();
 
         $parameters['format'] = 'xml';
         $parameters['do'] = 'getTagCloud';
 
-        $connectionData2->setAction('TagCloud.ff');
+        $request2->setAction('TagCloud.ff');
 
-        // This should load the content for $id2 as well
-        $this->dataProvider->loadResponse($id1);
-
-        $response = $connectionData1->getResponse();
+        // This should load the content for the second request as well
+        $response = $request1->getResponse();
         $this->assertEquals(0, $response->getConnectionErrorCode());
         $this->assertEquals('CURLE_OK', $response->getConnectionError());
         $this->assertEquals(200, $response->getHttpCode());
         $this->assertEquals($responseContent1, $response->getContent());
 
-        $response = $connectionData2->getResponse();
+        $response = $request2->getResponse();
         $this->assertEquals(0, $response->getConnectionErrorCode());
         $this->assertEquals('CURLE_OK', $response->getConnectionError());
         $this->assertEquals(200, $response->getHttpCode());
         $this->assertEquals($responseContent2, $response->getContent());
     }
 
-    // This is NOT how the MultiCurlDataProvider should be used in production.
+    // This is NOT how the MultiCurlRequestFactory should be used in production.
     // In fact, it will log a warning whenever it is used like this. If
-    // possible, always configure all connections first and then use
-    // loadResponse().
-    public function testLoadInSequence()
+    // possible, always configure all Requests first and then use getResponse().
+    public function testUseRequestsInSequence()
     {
         $this->configuration->makeHttpAuthenticationType();
 
         $requiredOptions1 = array(
-            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?format=json&do=getTagCloud&verbose=true&channel=de'
+            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?query=bmx&format=json&do=getTagCloud&verbose=true&channel=de'
         );
         $responseContent1 = 'test response 1';
         $info = array(
@@ -160,7 +154,7 @@ class MultiCurlDataProviderTest extends \FACTFinder\Test\BaseTestCase
         $this->curlStub->setInformation($info, $requiredOptions1);
 
         $requiredOptions2 = array(
-            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?format=xml&do=getTagCloud&verbose=true&channel=de'
+            CURLOPT_URL => 'http://user:userpw@demoshop.fact-finder.de:80/FACT-Finder/TagCloud.ff?query=bmx&format=xml&do=getTagCloud&verbose=true&channel=de'
         );
         $responseContent2 = 'test response 2';
         $info = array(
@@ -170,58 +164,48 @@ class MultiCurlDataProviderTest extends \FACTFinder\Test\BaseTestCase
         $this->curlStub->setResponse($responseContent2, $requiredOptions2);
         $this->curlStub->setInformation($info, $requiredOptions2);
 
-        $connectionData1 = FF::getInstance('Core\Server\ConnectionData');
-        $id1 = $this->dataProvider->register($connectionData1);
+        $request1 = $this->factory->getRequest();
 
-        $parameters = $connectionData1->getParameters();
+        $parameters = $request1->getParameters();
 
         $parameters['format'] = 'json';
         $parameters['do'] = 'getTagCloud';
 
-        $connectionData1->setAction('TagCloud.ff');
+        $request1->setAction('TagCloud.ff');
 
-        $connectionData2 = FF::getInstance('Core\Server\ConnectionData');
-        $id2 = $this->dataProvider->register($connectionData2);
+        $request2 = $this->factory->getRequest();
 
-        // This should not load the content for $id2, because it has not yet
-        // been configured.
-        $this->dataProvider->loadResponse($id1);
-
-        $response1 = $connectionData1->getResponse();
+        // This should not load the response for $request2, because it has not
+        // yet been configured.
+        $response1 = $request1->getResponse();
         $this->assertEquals(0, $response1->getConnectionErrorCode());
         $this->assertEquals('CURLE_OK', $response1->getConnectionError());
         $this->assertEquals(200, $response1->getHttpCode());
         $this->assertEquals($responseContent1, $response1->getContent());
 
         $this->assertInstanceOf('FACTFinder\Core\Server\NullResponse',
-                                $connectionData2->getResponse());
+                                $request2->getResponse());
 
         // Now configure second connection
-        $parameters = $connectionData2->getParameters();
+        $parameters = $request2->getParameters();
 
         $parameters['format'] = 'xml';
         $parameters['do'] = 'getTagCloud';
 
-        $connectionData2->setAction('TagCloud.ff');
+        $request2->setAction('TagCloud.ff');
 
-        // Calling loadResponse() for $id1 should not load the second
-        // connection, because there is nothing to be done for $id1 itself.
-        $this->dataProvider->loadResponse($id1);
-
-        $this->assertSame($response1, $connectionData1->getResponse());
-        $this->assertInstanceOf('FACTFinder\Core\Server\NullResponse',
-                                $connectionData2->getResponse());
+        // Calling getResponse() for $request should not do anything, because
+        // there is nothing to be done for $id1 itself.
+        $this->assertSame($response1,  $request1->getResponse());
 
         // This should now load the second response but should NOT reload the
         // first one.
-        $this->dataProvider->loadResponse($id2);
+        $response = $request2->getResponse();
+        $this->assertEquals(0, $response->getConnectionErrorCode());
+        $this->assertEquals('CURLE_OK', $response->getConnectionError());
+        $this->assertEquals(200, $response->getHttpCode());
+        $this->assertEquals($responseContent2, $response->getContent());
 
-        $this->assertSame($response1, $connectionData1->getResponse());
-
-        $response2 = $connectionData2->getResponse();
-        $this->assertEquals(0, $response2->getConnectionErrorCode());
-        $this->assertEquals('CURLE_OK', $response2->getConnectionError());
-        $this->assertEquals(200, $response2->getHttpCode());
-        $this->assertEquals($responseContent2, $response2->getContent());
+        $this->assertSame($response1, $request1->getResponse());
     }
 }
