b114f7f364910c38281e4a689d389cb054d86a16
Stefan Krenz <krenz@marmalade.de>
2016-03-02 16:35:42 +0100

    Ported build-release.ps1 to build-release.sh

--- build-release.sh
+++ build-release.sh
@@ -1,1 +1,39 @@
-# TODO: This still needs to be ported from build-release.ps1
+#!/usr/bin/env sh
+pushd $(dirname $(readlink -f $0)) > /dev/null
+
+# Read current branch
+GIT_BRANCH=$(git branch|grep -E '^\*'|awk '{ print $2 }')
+
+git checkout release
+
+# Read version
+APP_VERSION=$(grep -E '^Version' CHANGELOG.txt |sort -Vr|head -n1|awk '{ print $2 }')
+
+# Build and commit documentation
+phpdoc -d src/FACTFinder -t docs --template clean --sourcecode
+git add --all docs
+git commit -m "Update documentation for release ${APP_VERSION}"
+git push origin release
+
+# Prepare and push master branch
+git checkout master
+git merge --no-ff --log release
+git push origin master
+
+# Prepare and push src-only branch
+git subtree split --prefix=src --onto=src-only --branch=src-only
+git push origin src-only
+
+# Create release tags
+git tag ${APP_VERSION}
+git tag ${APP_VERSION}+src src-only
+
+git push --tags origin
+
+# Move back to original branch
+
+git checkout ${GIT_BRANCH}
+
+Pop-Location
+
+popd > /dev/null
